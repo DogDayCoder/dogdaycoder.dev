fffb987ae550f298c0bc2992e52c37978cfd8df5
Ben Wills <ben.wills@hotmail.com>
2024-11-22 17:26:16 +0000

    Initial trial of Mantine UI

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { NavigationPane } from '../shared/components/navigationPane/NavigationPane.tsx';
+
+const Home = () => {
+    return (
+        <div>
+            <NavigationPane />
+        </div>
+    );
+};
+
+export default Home;
